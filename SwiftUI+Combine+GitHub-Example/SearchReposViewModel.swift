a8fb1bd9ca3396d8047143ee0a25c13b078b7839
Kemal Enver <kemal.enver@qantasloyalty.com>
2019-06-21 09:15:26 +1000

    Better combine structure

--- SwiftUI+Combine+GitHub-Example/SearchReposViewModel.swift
+++ SwiftUI+Combine+GitHub-Example/SearchReposViewModel.swift
@@ -26,20 +26,29 @@ final class SearchReposViewModel: BindableObject {
         let path = Bundle.main.path(forResource: "repos_response", ofType: "json")!
         let url = URL(fileURLWithPath: path)
         
-        URLSession.shared.dataTaskPublisher(for: url)
-            .map({ (published) -> Data in
-                published.data
-            })
+        let publisher = URLSession.shared.dataTaskPublisher(for: url)
+        
+        publisher
+            .map { publisher -> Data in
+                publisher.data
+            }
+            
             .decode(type: ReposResponse.self, decoder: JSONDecoder())
-            .map({ (repResponse) -> [Repo] in
+            
+            .map { repResponse -> [Repo] in
                 repResponse.items
-            })
+            }
+            
+            .receive(on: RunLoop.main)
+            
             .replaceError(with: [])
-            .sink(receiveValue: { (repos) in
-                DispatchQueue.main.async {
-                    self.repos = repos
-                }
-            })
+            // Not sure why assign doesnt work yet...
+            //            .assign(to: \.repos, on: self)
+            .sink { (repos) in
+                self.repos = repos
+            }
+            // Swallows warning.  Not sure if this is correct way...
+            .receive(completion: Subscribers.Completion<Never>.finished)
     }
     
     func loadFromAPI(_ searchTerm: String) {
@@ -52,19 +61,28 @@ final class SearchReposViewModel: BindableObject {
         var request = URLRequest(url: urlComponents.url!)
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         
-        URLSession.shared.dataTaskPublisher(for: request)
-            .map({ (published) -> Data in
-                published.data
-            })
+        let publisher = URLSession.shared.dataTaskPublisher(for: request)
+        
+        publisher
+            .map { publisher -> Data in
+                publisher.data
+            }
+            
             .decode(type: ReposResponse.self, decoder: JSONDecoder())
-            .map({ (repResponse) -> [Repo] in
+            
+            .map { repResponse -> [Repo] in
                 repResponse.items
-            })
+            }
+            
+            .receive(on: RunLoop.main)
+            
             .replaceError(with: [])
-            .sink(receiveValue: { (repos) in
-                DispatchQueue.main.async {
-                    self.repos = repos
-                }
-            })
+            // Not sure why assign doesnt work yet...
+//            .assign(to: \.repos, on: self)
+            .sink { (repos) in
+                self.repos = repos
+            }
+            // Swallows warning.  Not sure if this is correct way...
+            .receive(completion: Subscribers.Completion<Never>.finished)
     }
 }
